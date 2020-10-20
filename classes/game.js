55fbcd6c990037169f4769ee0404f73e7e0a595c
hecht0r <59474578+hecht0r@users.noreply.github.com>
2020-10-20 12:26:03 +0200

    Initial Commit

--- classes/game.js
+++ classes/game.js
@@ -1,92 +1,105 @@
 const helper = require('./../helpers.js'); 
 const deck = require('./deck.js');
-const round = require('./round.js');
 const action = require('./../actions.js');
 
 module.exports = class Game{
-    constructor() {
-		this.rounds = [];
-		this.winner;
-    }
+    constructor(players) {
+		this.players = players.slice();
+		this.shoveCount = 0;
+		this.knockCount = 0;
+		this.moveCount = 0;
+	}
+	
+	getNextPlayer(player) {
+		let index = this.players.indexOf(player);
+		if (index == this.players.length-1){
+			index = -1;
+		}
+		return this.players[index + 1];
+	}
+
+	emitPlayers(event, data) {
+		for (let i = 0; i < this.players.length; i++) {
+			this.players[i].emit(event, data);
+		}
+	}
     
 	start(starter) {
-		// start game, create deck and draw trumpcard
+		// start game, create deck and deal cards
 		let m = helper.findMatchBySocketId(starter.socket.id);
 		console.log(starter.socket.username + ' beginnt.')
-		m.emitPlayers('newGame',starter.socket.username);
+		this.emitPlayers('newGame',starter.socket.username);
 		this.starter = starter;
 		this.deck = new deck();
 		this.deck.createDeck();
 		this.deck.shuffleDeck();
-		this.trumpcard = this.deck.cards.slice(-1)[0];
-				
-		for (let i = 0; i < m.teams.length; i++) {
-			m.teams[i].init();
+
+		// hand out cards and tell the players' client
+		for (let i = 0; i < this.players.length; i++) {
+			let player = this.players[i];
+			player.hand = this.deck.cards.splice(0,3);
+			player.handValue = helper.handValue(player.hand);
+			player.emit('updateHand', player.hand);
 		}
 
-		// hand out cards and tell the players' client	
-		for (let i = 0; i < m.players.length; i++) {
-			m.players[i].init();
-			m.players[i].hand = this.deck.cards.splice(0,5);
-			if (m.players[i] == starter){
-				m.players[i].emit('setStarthand', m.players[i].hand);
-			}else{
-				m.players[i].emit('updateHand', m.players[i].hand);
-			}	
+		// check if someone has already >= 31 handvalue
+		for (let i = 0; i < this.players.length; i++) {
+			let player = this.players[i];
+			if (player.handValue >= 31){
+				this.end(player);
+			}
+		}
+		this.starter.emit('yourStartTurn');
+	}
 
-			// check if the player can forfeit the game
-			if (action.checkForfeit(m.players[i].hand)){
-				m.players[i].emit('forfeit');
-			}			
+	end(winner){
+		let m = helper.findMatchBySocketId(winner.socket.id);
+		
+		// tell scores to everyone
+		for (let i = 0; i < this.players.length; i++) {
+			m.emitPlayers('results',{player: this.players[i].name, score: this.players[i].handValue});
 		}
 
-		m.emitPlayers('updateTrumpcard', {trumpcard: this.trumpcard});
-		
-		// start first round
-		let r = new round.FirstRound(this.starter);
-		this.rounds.push(r);
-		r.start();		
-	}
-	
-	getCurrentRound() {
-		return this.rounds[this.rounds.length - 1];
-	}
-
-	checkGigackel(){
-		let m = helper.findMatchByTeam(this.winner);
-		if (m.teams.filter(team => team.wonCards.length > 0).length === 1){
-			return true;
-		}
-		return false;
-	}
-
-	end(winnerTeam){
-		// increase winners wincount, tell everyone and start new game
-		
-		let m = helper.findMatchByTeam(winnerTeam);
-		this.winner = winnerTeam;
-		
-		if(this.checkGigackel()){
-			winnerTeam.wins += 2;
-			console.log(`${winnerTeam.name} gewinnt!`);
-			m.emitPlayers('gameOver', {winner: winnerTeam.name, gigackel: true, teams: m.teams});
+		// find and tell losers to everyone
+		let losers;
+		if (winner.handValue === 33){
+			losers = this.players.filter(p => (p != winner));
 		}else{
-			winnerTeam.wins += 1;
-			console.log(`${winnerTeam.name} gewinnt!`);
-			m.emitPlayers('gameOver', {winner: winnerTeam.name, gigackel: false, teams: m.teams});		
-		};		
-		
-		let scoreBoard =[];
-
-		for (let i = 0; i < m.teams.length; i++) {
-			scoreBoard.push({team: m.teams[i].name, score: m.teams[i].wins});
+			let players = this.players.slice();
+			players.sort((a,b) => (a.handValue > b.handValue) ? 1 : ((b.handValue > a.handValue) ? -1 : 0)); 
+			losers = m.players.filter(p => (p.handValue === players[0].handValue));
 		}
 
+		for (let i = 0; i < losers.length; i++) {
+			losers[i].score -= 1;
+			console.log(`${losers[i].name} verliert!`);
+			m.emitPlayers('losers', losers[i].name);
+			
+			// check if players swim
+			if (losers[i].score === 0){
+				m.emitPlayers('swim', losers[i].name);
+			}
+			
+			// check if players have to leave the game
+			if (losers[i].score < 0){
+				m.emitPlayers('out', losers[i].name);
+				let index = this.players.indexOf(losers[i]);
+				if (index > -1) {
+					this.players.splice(index, 1);
+				}				
+			}
+		}
+	
+		let scoreBoard =[];
+		for (let i = 0; i < m.players.length; i++) {
+			scoreBoard.push({player: m.players[i].name, score: m.players[i].score});
+		}
+
+		let nextStarter = this.getNextPlayer(this.starter);	
+		let players = this.players;
 		m.emitPlayers('updateScoreboard',scoreBoard);
-		let starter = this.starter;
 		setTimeout(function() {
-			m.startGame(m.getNextPlayer(starter));
+			m.startGame(players, nextStarter);
 		}, 5000);
 	}
-
 }	
