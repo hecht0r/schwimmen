746ded51a0133c5e49c6db1f3b499e9addff1be1
hecht0r <59474578+hecht0r@users.noreply.github.com>
2020-12-05 13:18:15 +0100

    bugfix

--- classes/game.js
+++ classes/game.js
@@ -1,140 +1,140 @@
-const helper = require('./../helpers.js'); 
+const helper = require('./../helpers.js');
 const deck = require('./deck.js');
 const action = require('./../actions.js');
 
-module.exports = class Game{
+module.exports = class Game {
     constructor(players) {
-		this.players = players.slice();
-		this.shoveCount = 0;
-		this.knockCount = 0;
-		this.moveCount = 0;
-	}
-	
-	getNextPlayer(player) {
-		let index = this.players.indexOf(player);
-		if (index == this.players.length-1){
-			index = -1;
-		}
-		return this.players[index + 1];
-	}
+        this.players = players.slice();
+        this.shoveCount = 0;
+        this.knockCount = 0;
+        this.moveCount = 0;
+    }
 
-	emitPlayers(event, data) {
-		for (let i = 0; i < this.players.length; i++) {
-			if (this.players[i].human){
-				this.players[i].emit(event, data);
-			}	
-		}
-	}
-    
-	start(starter) {
-		// start game, create deck and deal cards
-		let m = helper.findMatchBySocketId(starter.socket.id);
-		helper.log('---');
-		helper.log(starter.socket.username + ' beginnt.')
-		this.emitPlayers('newGame',starter.socket.username);
-		m.emitPlayers('updateScoreboard',m.getScoreboard());
-		this.starter = starter;
-		this.deck = new deck();
-		this.deck.createDeck();
-		this.deck.shuffleDeck();
+    getNextPlayer(player) {
+        let index = this.players.indexOf(player);
+        if (index == this.players.length - 1) {
+            index = -1;
+        }
+        return this.players[index + 1];
+    }
 
-		
-		// hand out cards and tell the players' client
-		for (let i = 0; i < this.players.length; i++) {
-			let player = this.players[i];
-			player.hand = this.deck.cards.splice(0,3);
-			player.handValue = helper.handValue(player.hand);
-			player.emit('updateHand', player.hand);
-		}
-		
-		// check if someone has already >= 31 handvalue
-		let startRound = true;
-		for (let i = 0; i < this.players.length; i++) {
-			let player = this.players[i];
-			if (player.handValue >= 31){
-				startRound = false;
-				this.end(player);
-			}
-		}
-		if (startRound){
-			this.starter.emit('yourStartTurn');
-		}
-	}
+    emitPlayers(event, data) {
+        for (let i = 0; i < this.players.length; i++) {
+            this.players[i].emit(event, data);
+        }
+    }
 
-	async end(winner){
-		let m = helper.findMatchBySocketId(winner.socket.id);
+    start(starter) {
+        // start game, create deck and deal cards
+        let m = helper.findMatchBySocketId(starter.socket.id);
+        helper.log('---');
+        helper.log(starter.socket.username + ' beginnt.')
+        this.emitPlayers('newGame', starter.socket.username);
+        m.emitPlayers('updateScoreboard', m.getScoreboard());
+        this.starter = starter;
+        this.deck = new deck();
+        this.deck.createDeck();
+        this.deck.shuffleDeck();
 
-		m.emitPlayers('roundOver');
-		
-		// tell scores to everyone
-		for (let i = 0; i < this.players.length; i++) {
-			m.emitPlayers('results',{player: this.players[i].socket.username, score: this.players[i].handValue});
-			helper.log(this.players[i].socket.username + ': ' + this.players[i].handValue);
-		}
 
-		// find and tell losers to everyone
-		let losers;
-		if (winner.handValue === 33){
-			losers = this.players.filter(p => (p != winner));
-		}else{
-			let players = this.players.slice();
-			players.sort((a,b) => (a.handValue > b.handValue) ? 1 : ((b.handValue > a.handValue) ? -1 : 0)); 
-			losers = this.players.filter(p => (p.handValue === players[0].handValue));
-		}
+        // hand out cards and tell the players' client
+        for (let i = 0; i < this.players.length; i++) {
+            let player = this.players[i];
+            player.hand = this.deck.cards.splice(0, 3);
+            player.handValue = helper.handValue(player.hand);
+            player.emit('updateHand', player.hand);
+        }
 
-		let playerLength = this.players.length;
-		for (let i = 0; i < losers.length; i++) {
-			let loser = losers[i];
-			loser.score -= 1;
-			helper.log(`${loser.socket.username} verliert!`);
-			m.emitPlayers('losers', loser.socket.username);
-			
-			// check if players swim
-			if (loser.score === 0){
-				m.emitPlayers('swim', loser.socket.username);
-			}
-			
-			// check if players have to leave the game, not if losers are the only active players
-			if (loser.score < 0){
-				if (losers.length === playerLength){
-					loser.score++;
-				}else{
-					loser.alive = false;
-					loser.score = '†';
-					m.emitPlayers('out', loser.socket.username);
-					let index = this.players.indexOf(loser);
-					if (index > -1) {
-						this.players.splice(index, 1);
-					}
-				}				
-			}
-		}
+        // check if someone has already >= 31 handvalue
+        let startRound = true;
+        for (let i = 0; i < this.players.length; i++) {
+            let player = this.players[i];
+            if (player.handValue >= 31) {
+                startRound = false;
+                this.end(player);
+            }
+        }
+        if (startRound) {
+            this.starter.emit('yourStartTurn');
+        }
+    }
 
-		let players;
-		let timeout = 10000;
-		if (this.players.length === 1){
-			// game is over, so raise winners wincount, reset all playerscores and start new round
-			m.emitPlayers('winner',this.players[0].socket.username)
-			players = m.players;
-			let winnerIndex = players.indexOf(this.players[0]);
-			players[winnerIndex].wins++;
-			for (let i = 0; i < players.length; i++) {
-				players[i].init();
-			}
-			global.roundTimeout = await new Promise(r => setTimeout(r, timeout));
-			timeout = 0;
-			m.emitPlayers('gameOver');
-		}else{
-			// game is not over, so start new round with all players alive
-			players = m.players.filter(player => player.alive === true);
-		}
+    async end(winner) {
+        let m = helper.findMatchBySocketId(winner.socket.id);
 
-		let nextStarter = this.getNextPlayer(this.starter);	
+        m.emitPlayers('roundOver');
 
-		m.emitPlayers('updateScoreboard',m.getScoreboard());
+        // tell scores to everyone
+        for (let i = 0; i < this.players.length; i++) {
+            m.emitPlayers('results', { player: this.players[i].socket.username, score: this.players[i].handValue });
+            helper.log(this.players[i].socket.username + ': ' + this.players[i].handValue);
+        }
 
-		global.roundTimeout = setTimeout(function() {
-			m.startGame(players, nextStarter);
-		}, timeout);
-	}
-}	
+        // find and tell losers to everyone
+        let losers;
+        if (winner.handValue === 33) {
+            losers = this.players.filter(p => (p != winner));
+        } else {
+            let players = this.players.slice();
+            players.sort((a, b) => (a.handValue > b.handValue) ? 1 : ((b.handValue > a.handValue) ? -1 : 0));
+            losers = this.players.filter(p => (p.handValue === players[0].handValue));
+        }
+
+        let swimmersCount = this.players.filter(p => (p.score === 0)).length;
+        for (let i = 0; i < losers.length; i++) {
+            let loser = losers[i];
+            loser.score -= 1;
+            helper.log(`${loser.socket.username} verliert!`);
+            m.emitPlayers('losers', loser.socket.username);
+
+            // check if players swim
+            if (loser.score === 0) {
+                m.emitPlayers('swim', loser.socket.username);
+            }
+
+            // check if players have to leave the game
+            if (loser.score < 0) {
+                // replay round if all swimmers are losers
+                if (losers.length === swimmersCount) {
+                    loser.score++;
+                    m.emitPlayers('replay');
+                } else {
+                    loser.alive = false;
+                    loser.score = '†';
+                    m.emitPlayers('out', loser.socket.username);
+                    let index = this.players.indexOf(loser);
+                    if (index > -1) {
+                        this.players.splice(index, 1);
+                    }
+                }
+            }
+        }
+
+        let players;
+        let timeout = 10000;
+        if (this.players.length === 1) {
+            // game is over, so raise winners wincount, reset all playerscores and start new round
+            m.emitPlayers('winner', this.players[0].socket.username)
+            players = m.players;
+            let winnerIndex = players.indexOf(this.players[0]);
+            players[winnerIndex].wins++;
+            for (let i = 0; i < players.length; i++) {
+                players[i].init();
+            }
+            global.roundTimeout = await new Promise(r => setTimeout(r, timeout));
+            timeout = 0;
+            m.emitPlayers('gameOver');
+        } else {
+            // game is not over, so start new round with all players alive
+            players = m.players.filter(player => player.alive === true);
+        }
+
+        let nextStarter = this.getNextPlayer(this.starter);
+
+        m.emitPlayers('updateScoreboard', m.getScoreboard());
+
+        global.roundTimeout = setTimeout(function() {
+            m.startGame(players, nextStarter);
+        }, timeout);
+    }
+}
--- public/scripts/sockets.js
+++ public/scripts/sockets.js
@@ -1,217 +1,222 @@
 // when client joined a match, emitted to only one client
 socket.on('userSet', function(data) {
-	username = data.username;
-	matchId = data.matchId;
-	$('.login.page').fadeOut();
-	$('.game.page').show();
-	$('.login.page').off('click');
-	document.body.appendChild(document.createTextNode('Hello ' + data.username));
+    username = data.username;
+    matchId = data.matchId;
+    $('.login.page').fadeOut();
+    $('.game.page').show();
+    $('.login.page').off('click');
+    document.body.appendChild(document.createTextNode('Hello ' + data.username));
 });
 
 // first client can start game, emitted to only one client
 socket.on('setStart', function() {
-	let div = document.getElementById('admin');
-	let btn = document.createElement('button');
-	btn.id = 'adminbutton'
-	btn.innerHTML = 'Start game';
-	btn.className = 'adminbutton';
-	btn.addEventListener('click', function () {
-		startGame();
-		return false;
-	});
-	div.appendChild(btn);
+    let div = document.getElementById('admin');
+    let btn = document.createElement('button');
+    btn.id = 'adminbutton'
+    btn.innerHTML = 'Start game';
+    btn.className = 'adminbutton';
+    btn.addEventListener('click', function() {
+        startGame();
+        return false;
+    });
+    div.appendChild(btn);
 });
 
 // when someone joined the match, emitted to all clients of the match
 socket.on('userJoined', function(data) {
-	write('gameLog', data + ' betritt das Spiel.');
+    write('gameLog', data + ' betritt das Spiel.');
 });
 
 // when start button was pushed
 socket.on('gameStarted', function() {
-	clear('nextMove');
-	setCountdown(10);
+    clear('nextMove');
+    setCountdown(10);
 });
 
 // when a new game starts, emitted to all clients of the match
 socket.on('newGame', function(data) {
-	removeActions();
-	clear('middleCards');
-	clear('nextMove');
-	write('gameLog', '------------------');
-	write('gameLog', data + ' beginnt das Spiel');
-	
-	// init middleCards
-	let card;
-	let middleCards = document.getElementById('middleCards');
-	for (let i = 0; i < 3; i++) {
-		card = document.createElement('img');
-		card.setAttribute('class','card');
-		card.setAttribute('src',`/images/cards/back.png`);
-		middleCards.appendChild(card);
-	};
+    removeActions();
+    clear('middleCards');
+    clear('nextMove');
+    write('gameLog', '------------------');
+    write('gameLog', data + ' beginnt das Spiel');
+
+    // init middleCards
+    let card;
+    let middleCards = document.getElementById('middleCards');
+    for (let i = 0; i < 3; i++) {
+        card = document.createElement('img');
+        card.setAttribute('class', 'card');
+        card.setAttribute('src', `/images/cards/back.png`);
+        middleCards.appendChild(card);
+    };
 });
 
 // update standings, emitted to all clients of the match   
 socket.on('updateScoreboard', function(data) {
-	clear('standings');
-	clear('standingsTotal');
-	let columns;
-	if (data.length > 4){
-		columns = "2";
-	}else{
-		columns = "1";
-	}
-	
-	document.getElementById('standings').style.columnCount = columns; 
-	document.getElementById('standingsTotal').style.columnCount = columns;
-	
-	let score;
-	for(let i = 0; i < data.length; i++){
-		if (data[i].score >= 0){
-			score = "I".repeat(data[i].score);
-		}else{
-			score = data[i].score;
-		}
-		write('standings', data[i].player + ': ' +  score);
-		write('standingsTotal', data[i].player + ': ' + data[i].wins);		
-	} 
+    clear('standings');
+    clear('standingsTotal');
+    let columns;
+    if (data.length > 4) {
+        columns = "2";
+    } else {
+        columns = "1";
+    }
+
+    document.getElementById('standings').style.columnCount = columns;
+    document.getElementById('standingsTotal').style.columnCount = columns;
+
+    let score;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].score >= 0) {
+            score = "I".repeat(data[i].score);
+        } else {
+            score = data[i].score;
+        }
+        write('standings', data[i].player + ': ' + score);
+        write('standingsTotal', data[i].player + ': ' + data[i].wins);
+    }
 });
 
 // update clients cards, emitted to one client after another
 socket.on('updateHand', function(data) {
-	clear('playerCards');
-	for (let i = 0; i < data.length; i++) {
-		let card = document.createElement('img');
-		card.setAttribute('class','card');
-		card.setAttribute('src',`/images/cards/${data[i].id}.jpg`);
-		card.setAttribute('onclick','selectCard(this,"' + data[i].id + '")');
-		let myCards = document.getElementById('playerCards');
-		myCards.appendChild(card);
-	};
+    clear('playerCards');
+    for (let i = 0; i < data.length; i++) {
+        let card = document.createElement('img');
+        card.setAttribute('class', 'card');
+        card.setAttribute('src', `/images/cards/${data[i].id}.jpg`);
+        card.setAttribute('onclick', 'selectCard(this,"' + data[i].id + '")');
+        let myCards = document.getElementById('playerCards');
+        myCards.appendChild(card);
+    };
 });
 
 // update game cards
 socket.on('updateMiddlecards', function(data) {
-	clear('middleCards');
-	let card;
-	let middleCards = document.getElementById('middleCards');
-	for (let i = 0; i < data.length; i++) {
-		card = document.createElement('img');
-		card.setAttribute('class','card');
-		card.setAttribute('src',`/images/cards/${data[i].id}.jpg`);
-		card.setAttribute('onclick','selectMiddleCard(this,"' + data[i].id + '")');
-		middleCards.appendChild(card);
-	};
-	card = document.createElement('img');
-	card.setAttribute('class','card');
-	card.setAttribute('src',`/images/cards/back.png`);
-	middleCards.appendChild(card);
+    clear('middleCards');
+    let card;
+    let middleCards = document.getElementById('middleCards');
+    for (let i = 0; i < data.length; i++) {
+        card = document.createElement('img');
+        card.setAttribute('class', 'card');
+        card.setAttribute('src', `/images/cards/${data[i].id}.jpg`);
+        card.setAttribute('onclick', 'selectMiddleCard(this,"' + data[i].id + '")');
+        middleCards.appendChild(card);
+    };
+    card = document.createElement('img');
+    card.setAttribute('class', 'card');
+    card.setAttribute('src', `/images/cards/back.png`);
+    middleCards.appendChild(card);
 })
 
 // show move in log
 socket.on('move', function(data) {
-	if (data.includes('klopft')){
-		writeBold('gameLog', data);
-	}else{
-		write('gameLog', data);
-	}
+    if (data.includes('klopft')) {
+        writeBold('gameLog', data);
+    } else {
+        write('gameLog', data);
+    }
 })
 
 // round is over
 socket.on('roundOver', function(data) {
-	removeActions();
-	clear('nextMove');
-	setCountdown(10);
+    removeActions();
+    clear('nextMove');
+    setCountdown(10);
 })
 
 // everyone shoved -> new middleCards
 socket.on('newMiddlecards', function(data) {
-	write('gameLog', 'Neue Karten in der Mitte');
+    write('gameLog', 'Neue Karten in der Mitte');
 })
 
 // game is over
 socket.on('gameOver', function(data) {
-	clear('gameLog');
+    clear('gameLog');
 })
 
 // gameresults
 socket.on('results', function(data) {
-	writeBold('gameLog', data.player + ': ' + data.score)
+    writeBold('gameLog', data.player + ': ' + data.score)
 })
 
 // show roundlosers in log
 socket.on('losers', function(data) {
-	write('gameLog', data + ' verliert');
+    write('gameLog', data + ' verliert');
 })
 
 // show swimmers in log
 socket.on('swim', function(data) {
-	writeBold('gameLog', data + ' schwimmt')
+    writeBold('gameLog', data + ' schwimmt')
 })
 
 // show gamewinner in log
 socket.on('winner', function(data) {
-	writeBold('gameLog', data + ' gewinnt das Spiel')
+    writeBold('gameLog', data + ' gewinnt das Spiel')
+})
+
+// show replaymessage in log
+socket.on('replay', function(data) {
+    writeBold('gameLog', 'Runde wird erneut gespielt')
 })
 
 // if a player is out, remove cards
 socket.on('out', function(data) {
-	
-	if(data == username){
-		writeError('gameLog', 'Du bist raus');
-		removeActions();
-		clear('middleCards');
-		clear('playerCards');
-		clear('nextMove');
-		
-		let div = document.getElementById('middleCards');
-		img = document.createElement('img');
-		img.setAttribute('class','gameover');
-		img.setAttribute('src','/images/gameover.png');
-		div.appendChild(img);
-	}else{
-		writeBold('gameLog', data + ' ist raus');
-	}
+
+    if (data == username) {
+        writeError('gameLog', 'Du bist raus');
+        removeActions();
+        clear('middleCards');
+        clear('playerCards');
+        clear('nextMove');
+
+        let div = document.getElementById('middleCards');
+        img = document.createElement('img');
+        img.setAttribute('class', 'gameover');
+        img.setAttribute('src', '/images/gameover.png');
+        div.appendChild(img);
+    } else {
+        writeBold('gameLog', data + ' ist raus');
+    }
 })
 
 // when it's clients turn to start, emitted to starter only
 socket.on('yourStartTurn', function() {
-	actionCanBeSent = true;
-	addActions('firstMove');
-	startTimerAutoKeep(60);
+    actionCanBeSent = true;
+    addActions('firstMove');
+    startTimerAutoKeep(60);
 })
 
 // when it's clients turn to start, emitted to starter only
 socket.on('yourTurnNoKnock', function() {
-	actionCanBeSent = true;
-	addActions('noKnock');
-	startTimerAutoShove(60);
+    actionCanBeSent = true;
+    addActions('noKnock');
+    startTimerAutoShove(60);
 })
 
 // when it's clients turn to play, emitted to one client after another
 socket.on('yourTurn', function() {
-	actionCanBeSent = true;
-	addActions('regular');
-	startTimerAutoShove(60);
+    actionCanBeSent = true;
+    addActions('regular');
+    startTimerAutoShove(60);
 })
 
 // tell all players whos turn it is, emitted to all clients of the game
 socket.on('nextPlayer', function(data) {
-	clear('nextMove');
-	if(data == username){
-		write('nextMove', 'Du bist dran');
-	}else{
-		write('nextMove', data + ' ist dran');
-	}
+    clear('nextMove');
+    if (data == username) {
+        write('nextMove', 'Du bist dran');
+    } else {
+        write('nextMove', data + ' ist dran');
+    }
 });
 
 // when a client disconnected, emitted to all clients of the match
 socket.on('playerDisconnected', function(data) {
-	write('gameLog', data + ' hat das Spiel verlassen');
+    write('gameLog', data + ' hat das Spiel verlassen');
 });
 
 // application error
 socket.on('error', function() {
-	writeError('gameLog', 'Anwendungsfehler - bitte Seite neu laden');
+    writeError('gameLog', 'Anwendungsfehler - bitte Seite neu laden');
 });
